0d12194318ad67ce27933b644c80fa78f24b0690
ZichengQu <quzicheng315@gmail.com>
2019-03-26 15:40:38 +1100

    Basic

---
+++ SES2A/src/com/action/UserAction.java
@@ -0,0 +1,13 @@
+package com.action;
+
+import com.opensymphony.xwork2.Action;
+
+public class UserAction implements Action {
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
