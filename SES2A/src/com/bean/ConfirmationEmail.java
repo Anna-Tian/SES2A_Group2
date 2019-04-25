3dcbf5734d326cc7317b46d8f1e146424e4752b4
ZichengQu <quzicheng315@gmail.com>
2019-04-25 14:04:09 +1000

    qqq

--- SES2A/src/com/bean/ConfirmationEmail.java
+++ SES2A/src/com/bean/ConfirmationEmail.java
@@ -20,6 +20,7 @@ public class ConfirmationEmail {
 	private String publishTime;
 	private String updateTime;
 	private String subject;
+	
 	public ConfirmationEmail() {
 		super();
 		// TODO Auto-generated constructor stub
