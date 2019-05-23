3fa3851b05ca9fd5166653b90b7070beea09c948
Huy <huysomething@gmail.com>
2019-05-23 22:32:24 +1000

    Changes to advisors (jsp,dao,servlet) All functions working

--- SES2A/src/com/dao/advisorDao.java
+++ SES2A/src/com/dao/advisorDao.java
@@ -13,79 +13,142 @@ import org.hibernate.cfg.Configuration;
 
 import com.bean.Advisor;
 
-
 public class advisorDao {
-	public void add(String staffNumber, String firstName, String lastName, String email ) {
-        try {
-            // 1. configuring hibernate
-            Configuration configuration = new Configuration().configure();
- 
-            // 2. create sessionfactory
-            SessionFactory sessionFactory = configuration.buildSessionFactory();
- 
-            // 3. Get Session object
-            Session session = sessionFactory.openSession();
- 
-            // 4. Starting Transaction
-            Transaction transaction = session.beginTransaction();
-            Advisor advisor = new Advisor();
-            advisor.setStaffNumber(staffNumber);
-            advisor.setFirstName(firstName);
-            advisor.setLastName(lastName);
-            advisor.setEmail(email);
-            session.save(advisor);
-            transaction.commit();
-            System.out.println("\n\n Details Added \n");
- 
-        } catch (HibernateException e) {
-            System.out.println(e.getMessage());
-            System.out.println("error");
-        }
-	}
-	public void update(int advisorId, String staffNumberupdate, String firstNameupdate, String lastNameupdate, String emailupdate) {
-        try {
-  		  Configuration cfg = new Configuration();
-  		  cfg.configure("hibernate.cfg.xml");
-  	  
-  		  SessionFactory factory = cfg.buildSessionFactory(); 
-  		  Session session =  factory.openSession();
-  	  
-  		  Object o= session.load(Advisor.class, advisorId); 
-  		  Advisor advisorupdate=(Advisor)o;
-  	  
-  		  Transaction transaction = session.beginTransaction();
-            advisorupdate.setStaffNumber(staffNumberupdate);
-            advisorupdate.setFirstName(firstNameupdate);
-            advisorupdate.setLastName(lastNameupdate);
-            advisorupdate.setEmail(emailupdate);
-            session.update(advisorupdate);
-            transaction.commit();
-            System.out.println("\n\n Details Updated \n");
- 
-        } catch (HibernateException e) {
-            System.out.println(e.getMessage());
-            System.out.println("error");
-        }
-   }
-	public void delete(int advisorId) {  
-		  Configuration cfg = new Configuration();
-		  cfg.configure("hibernate.cfg.xml");
-	  
-		  SessionFactory factory = cfg.buildSessionFactory(); 
-		  Session session =  factory.openSession();
-	  
-		  Object o= session.load(Advisor.class, advisorId); 
-		  Advisor a=(Advisor)o;
-	  
-		  Transaction transaction = session.beginTransaction(); 
-		  session.delete(a);
-		  System.out.println("Object Deleted successfully.....!!"); 
-		  transaction.commit();
-		  session.close(); 
-		  factory.close(); 
-		  }
- 
-	}
-//	public Advisor getAdvisor(int advisorId);
-//	public List getAllAdvisor();
+	public void add(String staffNumber, String firstName, String lastName, String email) {
+		try {
+			// Configuring hibernate
+			Configuration configuration = new Configuration().configure();
 
+			// Create sessionfactory
+			SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+			// Get Session object
+			Session session = sessionFactory.openSession();
+
+			// Starting Transaction
+			Transaction transaction = session.beginTransaction();
+			Advisor advisor = new Advisor();
+			advisor.setStaffNumber(staffNumber);
+			advisor.setFirstName(firstName);
+			advisor.setLastName(lastName);
+			advisor.setEmail(email);
+			advisor.setIsActive("Active");
+			session.save(advisor);
+			transaction.commit();
+			System.out.println("\n\n Details Added \n");
+
+		} catch (HibernateException e) {
+			System.out.println(e.getMessage());
+			System.out.println("error");
+		}
+	}
+
+	public void update(int advisorId, String staffNumberupdate, String firstNameupdate, String lastNameupdate,
+			String emailupdate) {
+		try {
+			// Configuring hibernate
+			Configuration cfg = new Configuration();
+			cfg.configure("hibernate.cfg.xml");
+
+			// Create sessionfactory
+			SessionFactory factory = cfg.buildSessionFactory();
+
+			// Get Session object
+			Session session = factory.openSession();
+			Object o = session.load(Advisor.class, advisorId);
+			Advisor advisorupdate = (Advisor) o;
+
+			// Starting Transaction
+			Transaction transaction = session.beginTransaction();
+			advisorupdate.setStaffNumber(staffNumberupdate);
+			advisorupdate.setFirstName(firstNameupdate);
+			advisorupdate.setLastName(lastNameupdate);
+			advisorupdate.setEmail(emailupdate);
+			advisorupdate.setIsActive("Active");
+			session.update(advisorupdate);
+			transaction.commit();
+			System.out.println("\n\n Details Updated \n");
+
+		} catch (HibernateException e) {
+			System.out.println(e.getMessage());
+			System.out.println("error");
+		}
+	}
+
+	public void delete(int advisorId) {
+		// Configuring hibernate
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+
+		// Create sessionfactory
+		SessionFactory factory = cfg.buildSessionFactory();
+		Session session = factory.openSession();
+
+		// Get Session object
+		Object o = session.load(Advisor.class, advisorId);
+		Advisor a = (Advisor) o;
+
+		// Starting Transaction
+		Transaction transaction = session.beginTransaction();
+		session.delete(a);
+		System.out.println("Object Deleted successfully.....!!");
+		transaction.commit();
+		session.close();
+		factory.close();
+	}
+
+	public void inactive(int advisorId) {
+		try {
+			// Configuring hibernate
+			Configuration cfg = new Configuration();
+			cfg.configure("hibernate.cfg.xml");
+
+			// Create sessionfactory
+			SessionFactory factory = cfg.buildSessionFactory();
+			Session session = factory.openSession();
+
+			// Get Session object
+			Object o = session.load(Advisor.class, advisorId);
+			Advisor advisorinactivate = (Advisor) o;
+
+			// Starting Transaction
+			Transaction transaction = session.beginTransaction();
+			advisorinactivate.setIsActive("InActive");
+			session.update(advisorinactivate);
+			transaction.commit();
+			System.out.println("\n\n Advisors Inactivated \n");
+
+		} catch (HibernateException e) {
+			System.out.println(e.getMessage());
+			System.out.println("error");
+		}
+	}
+
+	public void active(int advisorId) {
+		try {
+			// Configuring hibernate
+			Configuration cfg = new Configuration();
+			cfg.configure("hibernate.cfg.xml");
+
+			// Create sessionfactory
+			SessionFactory factory = cfg.buildSessionFactory();
+			Session session = factory.openSession();
+
+			// Get Session object
+			Object o = session.load(Advisor.class, advisorId);
+			Advisor advisoractivate = (Advisor) o;
+
+			// Starting Transaction
+			Transaction transaction = session.beginTransaction();
+			advisoractivate.setIsActive("Active");
+			session.update(advisoractivate);
+			transaction.commit();
+			System.out.println("\n\n Advisors Activate \n");
+
+		} catch (HibernateException e) {
+			System.out.println(e.getMessage());
+			System.out.println("error");
+		}
+	}
+
+}
--- SES2A/src/com/servlet/advisorServlet.java
+++ SES2A/src/com/servlet/advisorServlet.java
@@ -1,80 +1,108 @@
 package com.servlet;
- 
+
 import java.io.IOException;
- 
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
-
-import com.bean.Advisor;
 import com.dao.advisorDao;
- 
 
 public class advisorServlet extends HttpServlet {
-    
-	
-    private static final long serialVersionUID = 1L;
-    
-    protected void doPost(HttpServletRequest request,
-            HttpServletResponse response) throws ServletException, IOException {
-    	
-    	String action = request.getParameter("action");
-    	String[] checkbx = request.getParameterValues("chk");
-        String staffnumber = request.getParameter("staffnumberadd");
-        String firstname = request.getParameter("firstnameadd");
-        String lastname = request.getParameter("lastnameadd");
-        String email = request.getParameter("emailadd");
-        
-        String staffnumberupdate = request.getParameter("staffno");
-        String firstnameupdate = request.getParameter("fname");
-        String lastnameupdate = request.getParameter("lname");
-        String emailupdate = request.getParameter("staffemail");
-        if(action.equalsIgnoreCase("Add")) {
-        try {
-            advisorDao AdvisorDAO = new advisorDao();
-            AdvisorDAO.add(staffnumber, firstname, lastname, email);
-  		  System.out.println("Object Added successfully.....!!");
-            response.sendRedirect("AdvisorsTab.jsp");
-        	} catch (Exception e) {
-        		e.printStackTrace(); 
-        	}
-        }else if (action.equalsIgnoreCase("Update")) {
-        	if(checkbx != null) {
-            	Integer advisorId = Integer.valueOf(request.getParameter("adId"));
-                try {
-                    advisorDao AdvisorDAO = new advisorDao();
-                    AdvisorDAO.update(advisorId,staffnumberupdate, firstnameupdate, lastnameupdate, emailupdate);
-          		    System.out.println("Object Updated successfully.....!!");
-                    response.sendRedirect("AdvisorsTab.jsp");
-                } catch (Exception e) {
-         
-                    e.printStackTrace();
-                }
-        	}
 
-        }else if(action.equalsIgnoreCase("Delete")) {
-        	if(checkbx != null) {
-            	Integer advisorId = Integer.valueOf(request.getParameter("adId"));
-                try {
-                  advisorDao AdvisorDao = new advisorDao();
-                  AdvisorDao.delete(advisorId);
-        		  System.out.println("Object Deleted successfully.....!!"); 
-                  response.sendRedirect("AdvisorsTab.jsp");
+	private static final long serialVersionUID = 1L;
 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-                }catch (Exception e) {
-                        	e.printStackTrace();
-                     	}
-                	}
-        	}
-    	}
-    
+		String action = request.getParameter("action");
+		String[] checkbx = request.getParameterValues("chk");
+
+		if (action.equalsIgnoreCase("Add")) {
+			try {
+				for (int i = 1; i < 4; i++) {
+					String staffnumber = request.getParameter("staffnumberadd" + i);
+					String firstname = request.getParameter("firstnameadd" + i);
+					String lastname = request.getParameter("lastnameadd" + i);
+					String email = request.getParameter("emailadd" + i);
+	                if (!staffnumber.isEmpty() && !firstname.isEmpty() && !lastname.isEmpty() && !email.isEmpty() ) {
+						advisorDao AdvisorDAO = new advisorDao();
+						AdvisorDAO.add(staffnumber, firstname, lastname, email);
+						System.out.println("Advisors Added successfully.....!!");
+						response.sendRedirect("AdvisorsTab.jsp");
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+// Servlet action to get unique advisorId with the corresponding data to Update when action with value "Update" is called
+		} else if (action.equalsIgnoreCase("Update")) {
+			// Loop all ID that is checked using checkbox array
+			for (int i = 0; i < checkbx.length; i++) {
+
+				String staffnumberupdate = request.getParameter("staffno_" + checkbx[i]);
+				String firstnameupdate = request.getParameter("fname_" + checkbx[i]);
+				String lastnameupdate = request.getParameter("lname_" + checkbx[i]);
+				String emailupdate = request.getParameter("staffemail_" + checkbx[i]);
+				// Pass the data retrieved into UPDATE function in AdvisorDao
+				try {
+					advisorDao AdvisorDAO = new advisorDao();
+					AdvisorDAO.update(Integer.valueOf(checkbx[i]), staffnumberupdate, firstnameupdate, lastnameupdate,
+							emailupdate);
+					System.out.println("Advisors Updated successfully.....!!");
+					response.sendRedirect("AdvisorsTab.jsp");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+
+// Servlet action to get unique advisorId to Delete when action with value "Delete" is called
+		} else if (action.equalsIgnoreCase("Delete")) {
+			// Loop all ID that is checked using checkbox array
+			for (int i = 0; i < checkbx.length; i++) {
+				// Pass the data retrieved into DELETE function in AdvisorDao
+				try {
+					advisorDao AdvisorDao = new advisorDao();
+					AdvisorDao.delete(Integer.valueOf(checkbx[i]));
+					System.out.println("Advisors Deleted successfully.....!!");
+					response.sendRedirect("AdvisorsTab.jsp");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+
+// Servlet action to get unique advisorId to Inactivate Advisors when action with value "Inactivate" is called			
+		} else if (action.equalsIgnoreCase("Inactive")) {
+			// Loop all ID that is checked using checkbox array
+			for (int i = 0; i < checkbx.length; i++) {
+				// Pass the data retrieved into INACTIVE function in AdvisorDao
+				try {
+					advisorDao AdvisorDao = new advisorDao();
+					AdvisorDao.inactive(Integer.valueOf(checkbx[i]));
+					System.out.println("Advisors Inactivated successfully.....!!");
+					response.sendRedirect("AdvisorsTab.jsp");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+
+// Servlet action to get unique advisorId to Activate Advisors when action with value "Activate" is called			
+		} else if (action.equalsIgnoreCase("Active")) {
+			// Loop all ID that is checked using checkbox array
+			for (int i = 0; i < checkbx.length; i++) {
+				// Pass the data retrieved into ACTIVE function in AdvisorDao
+				try {
+					advisorDao AdvisorDao = new advisorDao();
+					AdvisorDao.active(Integer.valueOf(checkbx[i]));
+					System.out.println("Advisors Activated successfully.....!!");
+					response.sendRedirect("AdvisorsTab.jsp");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
